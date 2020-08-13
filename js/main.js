8a61fa64f4ac8fdf5b35a2bdeb615f6cbe227030
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-08-13 19:11:12 +0100

    results moved to p.textContent

--- js/main.js
+++ js/main.js
@@ -8,12 +8,16 @@ const words = document.querySelector(".words");
 words.appendChild(p);
 
 recognition.addEventListener("result", e => {
-    const transcript = [...e.results]
-    .map(result => result[0])
-    .map(result => result.transcript)
-    .join("")
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join("")
 
-    console.log(transcript);
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+        p = document.createElement("p");
+        words.appendChild(p);
+    }
 });
-
+recognition.addEventListener("end", recognition.start);
 recognition.start();
