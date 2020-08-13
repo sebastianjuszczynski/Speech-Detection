7fd40274beaeeca282e86c7b3ad301721117f590
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-08-13 19:03:28 +0100

    added event listener for result on recognition to create array with words

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+recognition.addEventListener("result", e => {
+    const transcript = [...e.results]
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("")
+
+    console.log(transcript);
+});
+
+recognition.start();
